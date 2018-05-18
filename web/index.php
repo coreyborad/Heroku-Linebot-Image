cbe4e9e0b99f94f5f953f221cca2518edbd97a6b
coreyborad <coreyborad@hotmail.com.tw>
2018-05-18 16:57:32 +0800

    [Master] 2018-05-18-2 1. Prevent too many request at same time,    to send too much msg

--- web/index.php
+++ web/index.php
@@ -1,15 +1,13 @@
 <?php
 require '../vendor/autoload.php';
 
+use Core\GetContent;
 use LINE\LINEBot;
 use LINE\LINEBot\Constant\HTTPHeader;
-use LINE\LINEBot\Event\MessageEvent;
-use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\Exception\InvalidEventRequestException;
 use LINE\LINEBot\Exception\InvalidSignatureException;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use Slim\App;
-use Core\GetContent;
 
 $app = new Slim\App([
     'settings' => [
@@ -23,7 +21,7 @@ $app = new Slim\App([
 ]);
 
 $app->post('/', function ($request, $response, $args) use ($app) {
-    $container = $app->getContainer();
+    $container  = $app->getContainer();
     $httpClient = new LINE\LINEBot\HTTPClient\CurlHTTPClient($container->settings['Line']['AccessToken']);
     $bot        = new LINE\LINEBot($httpClient, ['channelSecret' => $container->settings['Line']['SecretToken']]);
     $LadyPhotos = new Core\GetContent\LadyPhotos($container->settings['APIURL']);
@@ -40,7 +38,14 @@ $app->post('/', function ($request, $response, $args) use ($app) {
     } catch (InvalidEventRequestException $e) {
         return $request->withStatus(400, "Invalid event request");
     }
-    foreach ($events as $event) {
+    //同時最多只處理2筆event，避免洗訊息
+    if(count($events) > 2){
+        $event_count = 2;
+    }else{
+        $event_count = 1;
+    }
+    for ($i = 0; $i < $event_count; $i++) {
+        $event = $events[$i];
         switch (get_class($event)) {
             //收到文字訊息
             case 'LINE\LINEBot\Event\MessageEvent\TextMessage':
@@ -49,14 +54,14 @@ $app->post('/', function ($request, $response, $args) use ($app) {
                     case 'menu':
                         //傳遞按鈕
                         $actions = array(
-                            new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("來張表特美女圖", "lady_photo")
+                            new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("來張表特美女圖", "lady_photo"),
                         );
-                        $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("老司機選單","輸入menu可在叫我一次", "https://i.imgur.com/pjs79aM.jpg", $actions);
+                        $button                 = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("老司機選單", "輸入menu可在叫我一次", "https://i.imgur.com/pjs79aM.jpg", $actions);
                         $templateMessageBuilder = new LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("老司機選單", $button);
-                        $response          = $bot->replyMessage($event->getReplyToken(), $templateMessageBuilder);
+                        $response               = $bot->replyMessage($event->getReplyToken(), $templateMessageBuilder);
                         break;
                     case '5566':
-                        $img = $LadyPhotos->_getPhoto();
+                        $img               = $LadyPhotos->_getPhoto();
                         $imgMessageBUilder = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img, $img);
                         $response          = $bot->replyMessage($event->getReplyToken(), $imgMessageBUilder);
                         break;
@@ -67,7 +72,7 @@ $app->post('/', function ($request, $response, $args) use ($app) {
                 $postback = $event->getPostbackData();
                 switch ($postback) {
                     case 'lady_photo':
-                        $img = $LadyPhotos->_getPhoto();
+                        $img               = $LadyPhotos->_getPhoto();
                         $imgMessageBUilder = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img, $img);
                         $response          = $bot->replyMessage($event->getReplyToken(), $imgMessageBUilder);
                         break;
